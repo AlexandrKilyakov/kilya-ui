1c0f8424ac34f8b55abf07ba2b7b09a5e59d54c7
Александр <76633175+AlexandrKilyakov@users.noreply.github.com>
2025-12-24 02:07:05 +0300

    Feat: Готова первая версия библиотеки компанентов

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
 export { default as Button } from "./components/Buttons/Button";
 export { default as ButtonClose } from "./components/Buttons/ButtonClose";
 export { default as Input } from "./components/Input";
+export { default as Select } from "./components/Select";
+export { default as Range } from "./components/Range";
